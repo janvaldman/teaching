821b11b2499beb0f717a578b5d79d4c451a7ac72
Jan Valdman <36764599+matlabfem@users.noreply.github.com>
2023-06-24 05:49:08 +0200

    Add files via upload

---
+++ aproximace_pi.m
@@ -0,0 +1,12 @@
+n=15;                                              % maximalni pocet clenu
+p=[];
+for k=1:n
+    p=[(-1)^(k-1)*(1/(2*k-1)), 0, p];              % koeficienty polynomu
+    pi_k(k)=16*polyval(p,1/5)-4*polyval(p,1/239);  % aproximace pi
+end
+e_k=pi_k-pi;                                       % absolutni chyba 
+format long; 
+fprintf('  %2.0f:  %1.16f  %2.2d\n', [(1:n)' pi_k' abs(e_k)'].')
+semilogy(abs(e_k),'o-');                           % graf v log skale
+
+axis tight; enlarge_axis;
